30ffdcadd4a3ea4412ccea8dd2d9a5da95ac3fa9
Taras Taday <human.ttp@gmail.com>
2013-11-12 00:13:30 +0200

    view cleanup

--- fish-map-add-place/add_fishing_place_views.php
+++ fish-map-add-place/add_fishing_place_views.php
@@ -4,60 +4,52 @@
  * iconv("UTF-8", "ISO-8859-1//TRANSLIT", $text)
  */
 
-function add_fish_place_form($last_marker_id, $user_login, $fishes, $countries, $regions, $districts) {
+function add_fish_place_form($user_login, $fishes) {
     ?>
 
-    <div id="wrapper" style="text-align:center">
-        <div id="waiting" style="display: none;">
+    <div id="wrapper" >
+        <div id="waiting" >
             <div>Будь ласка, зачекайте, йде обробка...</div>
             <p>
                 <img src="<?php echo plugins_url('images/ajax-loader.gif', __FILE__) ?>"
-                     style="border:0" title="Waiting" alt="Waiting" />
+                      title="Waiting" alt="Waiting" />
             </p>
         </div>
-        <div id="add_place_result" style="display: none;"></div>
+        <div id="add_place_result" ></div>
 
-        <form id="add_place_form" name="add_place_form" method="POST">
+        <form id="add_place_form" method="POST" action="<?php echo admin_url('admin-ajax.php');?>">
+            <input type="hidden" name="action" value="fish_map_add_place_save" />
             <h4 title="Додати водойму на мапу">Додати водойму на мапу</h4>
             <div id="form_params">
-
-                    <div style="display: none">
-                        <strong>ID</strong>
-                        <small>(автоматичний лічільник)</small>
-                        <br/>&nbsp;&nbsp;&nbsp;
-                        <input type="text"
-                               id="column_marker_id"
-                               name="column_marker_id"
-                               value="<?php echo $last_marker_id ?>" size="20" readonly/>
-                        <br/><br/>
-                        <strong>Користувач</strong>
-                        <small>(логін користувача отримується автоматично)</small>
-                        <br/>&nbsp;&nbsp;&nbsp;
-                        <input type="text"
-                               id="column_user_login"
-                               name="column_user_login" value="<?php echo $user_login ?>" size ="62" readonly/>
-                        <br/><br/>
-                    </div>
+                <div >
+                    <strong>Користувач</strong>
+                    <small>(логін користувача отримується автоматично)</small>
+                    <br/>
+                    <input type="text"
+                           id="column_user_login"
+                           name="column_user_login" value="<?php echo $user_login ?>" readonly="1" />
+                    <br/><br/>
+                </div>
                 <label for="column_marker_name"><strong>Назва водойми</strong></label>
                 <small>(загально відома назва місця для рибалки, наприклад Біле Озеро, Бочаниця)</small>
-                <br/>&nbsp;&nbsp;&nbsp;
+                <br/>
                 <input id="column_marker_name"
                        type="text"
                        name="column_marker_name"
-                       value="" size="62" required/>
+                       value="" required="1" />
                 <br/><br/>
                 <label for="column_marker_lat"><strong>Координати на мапі</strong></label>
                 <small>(широта та довгота в одиницях Google Maps)</small>
-                <br/>&nbsp;&nbsp;&nbsp;
-                <input id="column_marker_lat" type="text" name="column_marker_lat" value="" size="31" required/>
-                <input id="column_marker_lng" type="text" name="column_marker_lng" value="" size="31" required/>
                 <br/>
-                <div id="map_canvas" style="width: 100%; height:300px; margin: 5px 0px;"></div>
+                <input id="column_marker_lat" type="text" name="column_marker_lat" value="" size="31" required="1" />
+                <input id="column_marker_lng" type="text" name="column_marker_lng" value="" required="1" />
+                <br/>
+                <div id="map_canvas" class="google-map" ></div>
                 <br/>
                 <label for="column_marker_permit"><strong>Умови рибалки</strong></label>
                 <small>(виберіть варіант зі списку)</small>
-                &nbsp;&nbsp;&nbsp;
-                <select id="column_marker_permit" name="column_marker_permit" size="1">
+
+                <select id="column_marker_permit" name="column_marker_permit">
                     <option value="unknown" selected>невідомо</option>
                     <option value="free">безкоштовно</option>
                     <option value="paid">платно</option>
@@ -66,27 +58,27 @@ function add_fish_place_form($last_marker_id, $user_login, $fishes, $countries, 
                 <br/><br/>
                 <label for="column_marker_contact"><strong>Контакт на водоймі</strong></label>
                 <small>(телефон та ім'я чи т.п., щоб замовити місце, просто поговорити, розпитати)</small>
-                <br/>&nbsp;&nbsp;&nbsp;
+                <br/>
                 <input id="column_marker_contact"
                        type="text"
                        name="column_marker_contact"
-                       value="" style="width:35em"/>
+                       value="" />
                 <br/><br/>
                 <label for="column_marker_paid_fish"><strong>Вартість риболовлі</strong></label>
                 <small>(ціна, час доби коли можна рибалити, вага риби на виніс і т.п.)</small>
-                <br/>&nbsp;&nbsp;&nbsp;
+                <br/>
                 <input id="column_marker_paid_fish"
                        type="text"
                        name="column_marker_paid_fish"
-                       value="" style="width:35em"/>
+                       value="" />
                 <br/><br/>
 
                 <label for="add_opts"><strong>Ввести додаткову інформацію:</strong></label>
                 <input type="checkbox" name="add_opts" id="add_opts" value="true"
                        onchange="$('#additional_opts').toggle();"/>
 
-                <div id="additional_opts" style="display:none">
-                    <div style="color:red; font-weight: bold; text-align: center">
+                <div id="additional_opts" >
+                    <div >
                         Дана форма на стідії розробки. Введені дані ігноруватимуться!</div>
                     <label for="fishes">Виберіть рибу</label>
                     <img src="<?php bloginfo('template_url'); ?>/images/info.gif"
@@ -94,7 +86,7 @@ function add_fish_place_form($last_marker_id, $user_login, $fishes, $countries, 
                          alt="Info"
                          title="|На даній водоймі ловляться такі риби" />
                     <small>(виберіть кілька риб тримаючи Ctrl та клацаючи на рибі)</small>
-                    <br/>&nbsp;&nbsp;&nbsp;
+                    <br/>
                     <select id="fishes" size="10" multiple="multiple" name="fishes[]">
                         <?php foreach ($fishes as $fish) { ?>
                             <option value="<?php echo $fish['fish_id'] ?>"><?php echo $fish['name'] ?></option>
@@ -105,82 +97,12 @@ function add_fish_place_form($last_marker_id, $user_login, $fishes, $countries, 
                     <label for="marker_address"><strong>Адреса водойми</strong></label>
                     <small>(наприклад "село Бочаниця, Гощанський район, Рівненська область")</small>
                     <br/>
-                    <input id="marker_address" type="text" name="address" value="" size="62"/>
-                    <br/><br/>
-                    <table>
-                        <tr>
-                            <td><label for="countries">Країна:</label></td>
-                            <td><label for="regions">Область:</label></td>
-                            <td><label for="districts">Район:</label></td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <select id="countries" size="4" name="countries[]" style="width:10em">
-                                    <option value="None" onclick="hideRegions(this)">невідомо</option>
-                                    <?php foreach ($countries as $country) { ?>
-                                        <option value="<?php echo $country['country_id'] ?>"
-                                                onclick="initRegions(this)">
-                                                    <?php echo $country['name'] ?>
-                                        </option>
-                                    <?php }
-                                    ?>
-                                </select>
-                            </td>
-                            <td>
-                                <select id="regions" size="4" name="regions[]" style="width:17em">
-                                    <option value="None" onclick="hideDistrict(this)">невідомо</option>
-                                    <?php foreach ($countries as $country) { ?>
-                                        <optgroup label="<?php echo $country['name'] ?>"
-                                                  id="country_<?php echo $country['country_id'] ?>"
-                                                  style="display: none">
-                                                      <?php
-                                                      foreach ($regions as $region) {
-                                                          if ($region['country_id'] == $country['country_id']) {
-                                                              ?>
-                                                    <option value="<?php echo $region['region_id'] ?>"
-                                                            onclick="initDistricts(this)">
-                                                    <?php echo $region['name'] ?>
-                                                    </option>
-                                                    <?php
-                                                }
-                                            }
-                                            ?>
-                                        </optgroup>
-                                    <?php }
-                                    ?>
-                                </select>
-                            </td>
-                            <td>
-                                <select id="districts" size="10" name="districts[]" style="width:17em">
-                                    <option value="None">невідомо</option>
-    <?php foreach ($regions as $region) { ?>
-                                        <optgroup label="<?php echo $region['name'] ?>"
-                                                  id="region_<?php echo $region['region_id'] ?>"
-                                                  style="display: none">
-                                                      <?php
-                                                      foreach ($districts as $district) {
-                                                          if ($district['region_id'] == $region['region_id']) {
-                                                              ?>
-                                                    <option value="<?php echo $district['district_id'] ?>">
-                                                    <?php echo $district['name'] ?>
-                                                    </option>
-                                                    <?php
-                                                }
-                                            }
-                                            ?>
-                                        </optgroup>
-    <?php }
-    ?>
-                                </select>
-                            </td>
-                        </tr>
-                    </table>
-                    <br/>
-                    <br/>
+                    <input id="marker_address" type="text" name="address" value="" />
+                    <br/><br/><br/>
                     <label for="marker_area"><strong>Площа водойми</strong></label>
                     <small>(в арах, 100 ар = 1 Га)</small>
                     <br/>
-                    <input id="marker_area" type="text" name="area" value="" size="62"/>
+                    <input id="marker_area" type="text" name="area" value=""/>
                     <br/>
                     <br/>
 
@@ -204,36 +126,36 @@ function add_fish_place_form($last_marker_id, $user_login, $fishes, $countries, 
                             <td><label for="marker_max_depth"><strong>Максимальна глибина</strong></label>
                                 <small>(в метрах)</small>
                                 <br/>
-                                <input id="marker_max_depth" type="text" name="max_depth" value="" size="62"/></td>
+                                <input id="marker_max_depth" type="text" name="max_depth" value="" ></td>
                             <td><label for="marker_average_depth"><strong>Середня глибина</strong></label>
                                 <small>(в метрах)</small>
                                 <br/>
-                                <input id="marker_average_depth" type="text" name="average_depth" value="" size="62"/></td>
+                                <input id="marker_average_depth" type="text" name="average_depth" value="" /></td>
                         </tr>
                         <tr>
                             <td><label id="marker_24h_price" for="marker_24h_price"><strong>Варість ловлі за добу</strong></label>
                                 <small>(в гривнях)</small>
                                 <br/>
-                                <input type="text" name="24h_price" value="" size="62"/>    </td>
+                                <input type="text" name="24h_price" value="" />    </td>
                             <td><label for="marker_dayhour_price"><strong>Варість ловлі за світловий день</strong></label>
                                 <small>(в гривнях)</small>
                                 <br/>
-                                <input id="marker_dayhour_price" type="text" name="dayhour_price" value="" size="62"/>    </td>
+                                <input id="marker_dayhour_price" type="text" name="dayhour_price" value="" />    </td>
                         </tr>
                     </table>
 
                     <label for="marker_boat_usage"><strong>Чи можна користуватись човном?</strong></label>
                     <small>(якщо поставите галочку, то мабуть можна)</small>
-                    &nbsp;&nbsp;&nbsp;
+
                     <input id="marker_boat_usage" type="checkbox" name="boat_usage" value=""/>
                     <br/>
                     <br/>
 
                     <label for="marker_boat_usage"><strong>Коли можна рибалити</strong></label>
                     <small>(виберіть варіант зі списку)</small>
-                    &nbsp;&nbsp;&nbsp;
-                    <select size="1">
-                            <option value="24h">цілодобово</option>
+
+                    <select>
+                        <option value="24h">цілодобово</option>
                         <option value="daylight">лише світловий день</option>
                     </select>
                     <br/>
