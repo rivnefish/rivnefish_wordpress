1ac78124b92c69a508775b6f6a6b04aff5a478d8
stal1n274 <stal1n@iddqd.in>
2014-01-12 00:25:39 +0200

    fixed Ukranian text. Again.

--- themes/Explorable/functions.php
+++ themes/Explorable/functions.php
@@ -898,7 +898,7 @@ if ( function_exists( 'get_custom_header' ) ) {
     <?php
             add_filter('the_content', 'subscribe_rss_text');
 	    function subscribe_rss_text($content){
-	    $subscribe_rss_text = '<p><i>Якщо сподобався допис — підпишись на <a href="http://feeds.feedburner.com/rivnefish/qKIS">RSS стрічку</a> O O або отримуй оновлення <a href="http://feedburner.google.com/fb/a/mailverify?uri=rivnefish/qKIS&loc=ru_RU">на email</a></i></p>';
+	    $subscribe_rss_text = '<p><i>Якщо сподобався допис — підпишись на <a href="http://feeds.feedburner.com/rivnefish/qKIS">RSS стрічку</a>  або отримуй оновлення <a href="http://feedburner.google.com/fb/a/mailverify?uri=rivnefish/qKIS&loc=ru_RU">на email</a></i></p>';
   	    return $content;
 	    }
     ?>
